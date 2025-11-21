dc21b17aa0a63890d6915c884a32e3ee9575d5c1
fishMonorail <fishmonorail@proton.me>
2025-11-20 19:28:06 -0600

    v

--- docs/scripts/index/hints.js
+++
@@ -1,8 +0,0 @@
-var hint = [
-  "Vielleicht is mobile friendly. You can view on the go!",
-  "Various easter eggs can be found throughout the site, be sure to hover over EVERYTHING!",
-  "Vielleicht is best viewed on a PC."
-];
-
-document.getElementById("hint").innerHTML =
-  hint[Math.floor(Math.random() * hint.length)];
