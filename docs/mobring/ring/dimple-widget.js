040f40d3486f0889709e6097eb6f0d42cff6fb3a
fishMonorail <fishmonorail@proton.me>
2025-11-22 11:27:55 -0600

    this better fucking work

--- docs/mobring/ring/dimple-widget.js
+++ docs/mobring/ring/dimple-widget.js
@@ -1,84 +1,89 @@
-function loadWebring() {
-    var tag = document.getElementById(ringID);
-    if (!tag) return;
+// onionring.js is made up of four files - onionring-widget.js (this one!), onionring-index.js, onionring-variables.js and onionring.css
+// it's licensed under the cooperative non-violent license (CNPL) v4+ (https://thufie.lain.haus/NPL.html)
+// it was originally made by joey + mord of allium (Ã¨â€™Å“) house, last updated 2020-11-24
 
-    var thisSite = window.location.href.replace(/\/$/, ''); // remove trailing slash
-    var thisIndex = null;
+// === ONIONRING-WIDGET ===
+//this file contains the code which builds the widget shown on each page in the ring. ctrl+f 'EDIT THIS' if you're looking to change the actual html of the widget
 
-    fetch('https://vielleicht.lol/mobring/ring/members.json')
-        .then(res => {
-            if (!res.ok) throw new Error("Failed to load member list");
-            return res.json();
-        })
-        .then(data => {
-            const sites = data.map(member => member.url.replace(/\/$/, '')); // normalize URLs
+var tag = document.getElementById(ringID); //find the widget on the page
 
-            for (let i = 0; i < sites.length; i++) {
-                if (thisSite.startsWith(sites[i])) {
-                    thisIndex = i;
-                    break;
-                }
-            }
+thisSite = window.location.href; //get the url of the site we're currently on
+thisIndex = null;
 
-            function randomSite() {
-                let otherSites = sites.slice();
-                if (thisIndex !== null) otherSites.splice(thisIndex, 1);
-                let randomIndex = Math.floor(Math.random() * otherSites.length);
-                location.href = otherSites[randomIndex];
-            }
-
-            if (thisIndex === null) {
-                tag.innerHTML = `
-                <div id="d-w" style="width:90px;text-align:center;">
-                    <table id="dimp">
-                        <tr>
-                            <td class='webring-info'>
-                                <a href='${indexPage}' target="_top">
-                                    <img src="https://vielleicht.lol/mobring/assets/dimp1.png">
-                                </a>
-                            </td>
-                        </tr>
-                    </table>
-                    <p>Not yet a member of the Psycho Helmet Webring</p>
-                </div>`;
-                return;
-            }
-
-            let prevIndex = (thisIndex - 1 + sites.length) % sites.length;
-            let nextIndex = (thisIndex + 1) % sites.length;
-
-            let indexLink = useIndex ? `<a href='${indexPage}'>index</a> | ` : '';
-            let randomLink = useRandom ? `<a href='javascript:void(0)' onclick='randomSite()'>random</a> | ` : '';
-
-            tag.innerHTML = `
-            <div id="d-w" style="width:90px;text-align:center;">
-                <table id="dimp">
-                    <tr>
-                        <td class='webring-prev'>
-                            <a href='${sites[prevIndex]}' target="_top">
-                                <img src="https://vielleicht.lol/mobring/assets/dimp2.png">
-                            </a>
-                        </td>
-                        <td class='webring-info'>
-                            <a href='${indexPage}' target="_top">
-                                <img src="https://vielleicht.lol/mobring/assets/dimp1.png">
-                            </a>
-                        </td>
-                        <td class='webring-next'>
-                            <a href='${sites[nextIndex]}' target="_top">
-                                <img src="https://vielleicht.lol/mobring/assets/dimp1.png">
-                            </a>
-                        </td>
-                    </tr>
-                </table>
-                <p>${indexLink}${randomLink}<a href='${indexPage}' target="_top">Psycho Helmet Webring</a></p>
-            </div>`;
-        })
-        .catch(err => {
-            console.error("Error loading webring members:", err);
-            if (tag) tag.innerHTML = `<p>Failed to load webring data</p>`;
-        });
+// go through the site list to see if this site is on it and find its position
+for (i = 0; i < sites.length; i++) {
+    if (thisSite.startsWith(sites[i])) { //we use startswith so this will match any subdirectory, users can put the widget on multiple pages
+        thisIndex = i;
+        break; //when we've found the site, we don't need to search any more, so stop the loop
+    }
 }
 
-// Wait until DOM is loaded
-window.addEventListener('DOMContentLoaded', loadWebring);
+function randomSite() {
+    otherSites = sites.slice(); //create a copy of the sites list
+    otherSites.splice(thisIndex, 1); //remove the current site so we don't just land on it again
+    randomIndex = Math.floor(Math.random() * otherSites.length);
+    location.href = otherSites[randomIndex];
+}
+
+//if we didn't find the site in the list, the widget displays a warning instead
+if (thisIndex == null) {
+    tag.insertAdjacentHTML('afterbegin', `
+ <div id="d-w" style="width: 90px;text-align: center;">
+     <table id="dimp">
+    <tr>
+     
+      <td class='webring-info'><a href='${indexPage}' target="_top"><img src="https://vielleicht.lol/mobring/assets/dimp1.png"></a></td>
+
+      
+     </tr>
+   
+      
+  
+  </table>
+   <p style="margin-top: 0;">
+     Not yet a member of the Psycho Helmet Webring
+   </p>
+   </div>
+  `);
+}
+else {
+    //find the 'next' and 'previous' sites in the ring. this code looks complex
+    //because it's using a shorthand version of an if-else statement to make sure
+    //the first and last sites in the ring join together correctly
+    previousIndex = (thisIndex - 1 < 0) ? sites.length - 1 : thisIndex - 1;
+    nextIndex = (thisIndex + 1 >= sites.length) ? 0 : thisIndex + 1;
+
+    indexText = ""
+    //if you've chosen to include an index, this builds the link to that
+    if (useIndex) {
+        indexText = `<a href='${indexPage}'>index</a> | `;
+    }
+
+    randomText = ""
+    //if you've chosen to include a random button, this builds the link that does that
+    if (useRandom) {
+        randomText = `<a href='javascript:void(0)' onclick='randomSite()'>random</a> | `;
+    }
+
+    //this is the code that displays the widget - EDIT THIS if you want to change the structure
+    tag.insertAdjacentHTML('afterbegin', `
+   <div id="d-w" style="width: 90px;text-align: center;">
+     <table id="dimp">
+    <tr>
+      <td class='webring-prev'><a href='${sites[previousIndex]}' target="_top"><img src="https://vielleicht.lol/mobring/assets/dimp2.png"></a></td>
+      <td class='webring-info'><a href='${indexPage}' target="_top"><img src="https://vielleicht.lol/mobring/assets/dimp1.png"></a></td>
+      <td class='webring-next'><a href='${sites[nextIndex]}' target="_top"><img src="https://vielleicht.lol/mobring/assets/dimp1.png"></a></td>
+      
+     </tr>
+   
+      
+  
+  </table>
+   <p style="margin-top: 0;">
+     <a href='${indexPage}' target="_top">Psycho Helmet Webring</a>
+   </p>
+   </div>
+  `);
+
+}
+}
--- docs/mobring/ring/onionring-variables.js
+++ docs/mobring/ring/onionring-variables.js
@@ -1,5 +1,16 @@
-// Ring settings
-var ringID = 'mobring';
-var indexPage = 'https://vielleicht.lol/mobring';
-var useIndex = true;
-var useRandom = false;
+// full URLs of all the sites in the ring (used by the widget)
+var sites = [
+    'https://vielleicht.lol'
+];
+
+// detailed metadata for each site (can be used elsewhere)
+var siteData = [
+    {
+        "url": "https://vielleicht.lol",
+        "websiteName": "Vielleicht",
+        "name": "Leicht",
+        "favs": "Tome, Reigen. join the webring pls",
+        "buttonUrl": "assets/buttons/nobutton.png",
+        "date": "2025-11-22"
+    }
+];
