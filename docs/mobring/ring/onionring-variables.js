f62fa3bad2b007ae0af6ab287c1baa8ff80d0886
fishMonorail <fishmonorail@proton.me>
2025-11-22 11:35:44 -0600

    crashing out again

--- docs/mobring/ring/onionring-variables.js
+++ docs/mobring/ring/onionring-variables.js
@@ -1,9 +1,15 @@
-// full URLs of all the sites in the ring (used by the widget)
+// onionring.js is made up of four files - onionring-widget.js, onionring-index.js, onionring-variables.js (this one!), and onionring.css
+// it's licensed under the cooperative non-violent license (CNPL) v4+ (https://thufie.lain.haus/NPL.html)
+// it was originally made by joey + mord of allium (ÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â¨ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â‚¬Å¡Ã‚Â¬ÃƒÂ¢Ã¢â‚¬Å¾Ã‚Â¢ÃƒÆ’Ã¢â‚¬Â¦ÃƒÂ¢Ã¢â€šÂ¬Ã…â€œ) house, last updated 2020-11-24
+
+// === ONIONRING-VARIABLES ===
+// this file contains the stuff you edit to set up your specific webring
+
+// full URLs of all the sites in the ring
 var sites = [
     'https://vielleicht.lol'
 ];
 
-// detailed metadata for each site (can be used elsewhere)
 var siteData = [
     {
         "url": "https://vielleicht.lol",
@@ -14,3 +20,19 @@ var siteData = [
         "date": "2025-11-22"
     }
 ];
+
+// the name of the ring
+var ringName = 'Mobring';
+
+/* the unique ID of the widget. two things to note:
+ 1) make sure there are no spaces in it - use dashes or underscores if you must
+ 2) remember to change 'webringid' in the widget code you give out and all instances of '#webringid' in the css file to match this value! */
+var ringID = 'mobring';
+
+// should the widget include a link to an index page?
+var useIndex = true;
+// the full URL of the index page. if you're not using one, you don't have to specify anything here
+var indexPage = 'https://vielleicht.lol/mobring';
+
+// should the widget include a random button?
+var useRandom = false;
